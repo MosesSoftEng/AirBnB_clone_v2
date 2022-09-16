70d7fd9abf0dc2cc17bc08d8c2aa4b56728cf938
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-16 09:15:15 +0300

    Refactor test

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,24 +2,21 @@
 """Unittests for console.py"""
 
 import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
 
 
 class TestHBNBCommand(unittest.TestCase):
     """Test HBNB console"""
 
+    @classmethod
+    def setUpClass(cls):
+        """HBNBCommand testing setup"""
+        cls.HBNB = HBNBCommand()
+
     def test_emptyline(self):
-    """Test empty line input."""
-    with patch("sys.stdout", new=StringIO()) as f:
-        self.HBNB.onecmd("\n")
-        self.assertEqual("", f.getvalue())
-
-    def test_quit(self):
-        """Test quit command input."""
+        """Test empty line input."""
         with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("quit")
+            self.HBNB.onecmd("\n")
             self.assertEqual("", f.getvalue())
-
-    def test_EOF(self):
-        """Test that EOF quits."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.assertTrue(self.HBNB.onecmd("EOF"))
