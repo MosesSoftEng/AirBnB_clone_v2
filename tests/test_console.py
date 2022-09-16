95f3fc2366fb122d12cfeec18e26b2880527889c
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-16 06:45:38 +0300

    Add unit test for console quit command

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,8 +7,14 @@ import unittest
 class TestHBNBCommand(unittest.TestCase):
     """Test HBNB console"""
 
-        def test_emptyline(self):
-        """Test empty line input."""
+    def test_emptyline(self):
+    """Test empty line input."""
+    with patch("sys.stdout", new=StringIO()) as f:
+        self.HBNB.onecmd("\n")
+        self.assertEqual("", f.getvalue())
+
+    def test_quit(self):
+        """Test quit command input."""
         with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("\n")
+            self.HBNB.onecmd("quit")
             self.assertEqual("", f.getvalue())
