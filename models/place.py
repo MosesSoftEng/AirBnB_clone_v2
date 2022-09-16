550c8329c9e6038290cfe8235439331fd8a6c967
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-16 07:24:06 +0300

    Refactor if condition

--- models/place.py
+++ models/place.py
@@ -35,48 +35,32 @@ class Place(BaseModel, Base):
         amenity_ids (list (of string)): list of Amenity.id of place.
     """
 
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "places"
+    __tablename__ = "places"
 
-        city_id = Column(String(60), ForeignKey("cities.id",
-                         ondelete='CASCADE'), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id",
-                         ondelete='CASCADE'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        reviews = relationship('Review', backref='place',
-                               cascade='all, delete-orphan',
-                               passive_deletes=True)
-        amenities = relationship('Amenity', backref='place_amenities',
-                                 cascade='all, delete',
-                                 secondary=place_amenity,
-                                 viewonly=False,
-                                 passive_deletes=True)
-
-    else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
-
-    def __init__(self, *args, **kwargs):
-        """initializes Place"""
-        super().__init__(*args, **kwargs)
+    city_id = Column(String(60), ForeignKey("cities.id",
+                        ondelete='CASCADE'), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id",
+                        ondelete='CASCADE'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
     if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        reviews = relationship('Review', backref='place',
+                            cascade='all, delete-orphan',
+                            passive_deletes=True)
+        amenities = relationship('Amenity', backref='place_amenities',
+                                    cascade='all, delete',
+                                    secondary=place_amenity,
+                                    viewonly=False,
+                                    passive_deletes=True)
+
         @property
         def reviews(self):
             """Returns the list of Review instances with place_id equals to
