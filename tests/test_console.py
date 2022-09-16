39618676a8dbffa8177e34fe3ee5172a2e9d2698
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-16 06:44:30 +0300

    Add unit test for emptyline

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,3 +6,9 @@ import unittest
 
 class TestHBNBCommand(unittest.TestCase):
     """Test HBNB console"""
+
+        def test_emptyline(self):
+        """Test empty line input."""
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("\n")
+            self.assertEqual("", f.getvalue())
