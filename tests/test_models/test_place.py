3c5cf9a6e8c8087e9d7083d0a7a51355f5773bcc
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-16 07:32:59 +0300

    Add pep8 test for models/place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -67,3 +67,9 @@ class test_Place(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.amenity_ids), list)
+
+    def test_pep8_Place(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/place.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
