e348a9beddb3dbc2e4fa364db07cd2ef1491c95e
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-29 14:39:02 +0300

    Edit comment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+# Fabfile to distribute an archive to a web server
 import os.path
 from fabric.api import env
 from fabric.api import put
@@ -10,11 +10,6 @@ env.hosts = ["3.235.227.53", "3.235.251.139"]
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
