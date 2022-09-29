6d478873daf387622aadd7317950feefd558c78a
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-29 11:49:20 +0300

    Add nginx redirect command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,7 @@ sudo apt install -y nginx
 # Create default nginx folder for pages
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /var/www/html/
 
 # Create index.html file and 
 sudo touch /data/web_static/releases/test/index.html
@@ -28,11 +29,26 @@ echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 
+# Create custom_404.html file
+sudo touch /var/www/html/custom_404.html
+
+# Change file ownership to current user to allow editing
+sudo chown -R "$USER":"$USER" /var/www/html/custom_404.html
+
+# Overwite file content
+echo "Ceci n'est pas une page" > /var/www/html/custom_404.html
+
 # Setup custom http response header
 sudo sed -i "s/listen \[::\]:80 default_server;/&\n\tadd_header X-Served-By $HOSTNAME;/" /etc/nginx/sites-enabled/default
 
+# Custom error page
+sudo sed -i "s/server_name _;/&\n\terror_page 404 \/custom_404.html;\n\tlocation \/custom_404.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}\n/" /etc/nginx/sites-enabled/default
+
+# Redirection
+sudo sed -i "s/server_name _;/&\n\tlocation \/redirect_me {\n\t\treturn 301 http:\/\/github.com\/;\n\t}\n/" /etc/nginx/sites-enabled/default
+
 # Create an endpoint to index.html
-sudo sed -i "s/server_name _;/&\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t\tindex index.html;\n\t}/" /etc/nginx/sites-enabled/default
+sudo sed -i "s/server_name _;/&\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t\tindex index.html;\n\t}\n/" /etc/nginx/sites-enabled/default
 
 # Restart nginx server
 sudo service nginx restart
