644f95dac3f1fb88b4a7dc081de0180b497dccbf
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-29 14:54:55 +0300

    Code changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,8 +9,12 @@ env.hosts = ["3.235.227.53", "3.235.251.139"]
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to a web server.
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
