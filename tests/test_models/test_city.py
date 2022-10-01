ab4b6bcca69e056d77a2343a7a577e1638fb3e6c
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-01 08:35:29 +0300

    Refactor city model unit test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,10 +2,13 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import pep8
 
 
 class test_City(test_basemodel):
-    """ """
+    """
+    Tests for City class instances.
+    """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -13,12 +16,21 @@ class test_City(test_basemodel):
         self.name = "City"
         self.value = City
 
-    # def test_state_id(self):
-    #     """ """
-    #     new = self.value()
-    #     self.assertEqual(type(new.state_id), str)
+    def test_state_id(self):
+        """
+        Tes if city state_id is of type string
+        """
+        new = self.value(name=self.name, state_id='test_id')
+        self.assertEqual(type(new.state_id), str)
 
-    # def test_name(self):
-    #     """ """
-    #     new = self.value()
-    #     self.assertEqual(type(new.name), str)
+    def test_name(self):
+        """
+        Test if city name is of type string
+        """
+        new = self.value(name=self.name)
+        self.assertEqual(type(new.name), str)
+
+    def test_pep8(self):
+        """Check pep8 styling"""
+        p = pep8.StyleGuide(quiet=True).check_files(["models/city.py"])
+        self.assertEqual(p.total_errors, 0)
