b440ca359fc8c24b513f4c1343c82699978910c3
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-01 09:24:13 +0300

    Add doc for test methods

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" """
+"""State model tests
+"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """State model test"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """State model test"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     # def test_name3(self):
-    #     """ """
+    #     """State model test"""
     #     new = self.value()
     #     self.assertEqual(type(new.name), str)
