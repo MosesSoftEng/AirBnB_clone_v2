62b1d8a1d1ed8b593b15d9834148f1f1e3be4c54
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-01 07:30:04 +0300

    Test correction

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,15 +5,20 @@ from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
-
+    """
+    Tests for Amemnity class instances.
+    """
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Constructor to setup test parameters
+        """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
-    # def test_name2(self):
-    #     """ """
-    #     new = self.value()
-    #     self.assertEqual(type(new.name), str)
+    def test_name2(self):
+        """
+        Test if name is of type string
+        """
+        new = self.value(name=self.name)
+        self.assertEqual(type(new.name), str)
