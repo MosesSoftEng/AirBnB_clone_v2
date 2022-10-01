fafa44fb7d90632a5e6537e5e6c3a3f091e1581d
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-01 09:34:27 +0300

    Add module methods docs

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,27 +8,27 @@ class test_User(test_basemodel):
     """User model unit test class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """User model test"""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     # def test_first_name(self):
-    #     """ """
+    #     """User model test"""
     #     new = self.value()
     #     self.assertEqual(type(new.first_name), str)
 
     # def test_last_name(self):
-    #     """ """
+    #     """User model test"""
     #     new = self.value()
     #     self.assertEqual(type(new.last_name), str)
 
     # def test_email(self):
-    #     """ """
+    #     """User model test"""
     #     new = self.value()
     #     self.assertEqual(type(new.email), str)
 
     # def test_password(self):
-    #     """ """
+    #     """User model test"""
     #     new = self.value()
     #     self.assertEqual(type(new.password), str)
