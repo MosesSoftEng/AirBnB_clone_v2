20922b8598deeebb497c05566dea7422ae791ec5
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-01 09:18:42 +0300

    Add test method docs

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-""" """
+"""Review mode test
+"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """Review mode test class
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Review model test"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     # def test_place_id(self):
-    #     """ """
+    #     """Review model test"""
     #     new = self.value()
     #     self.assertEqual(type(new.place_id), str)
 
     # def test_user_id(self):
-    #     """ """
+    #     """Review model test"""
     #     new = self.value()
     #     self.assertEqual(type(new.user_id), str)
 
     # def test_text(self):
-    #     """ """
+    #     """Review model test"""
     #     new = self.value()
     #     self.assertEqual(type(new.text), str)
