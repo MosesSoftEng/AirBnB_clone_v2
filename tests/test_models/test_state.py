dfaf766a848437c7dc54327ba6e9a9ab2565370b
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-01 09:25:45 +0300

    Add test for pep8 check

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -18,3 +18,8 @@ class test_state(test_basemodel):
         """State model test"""
         new = self.value(name="Texas")
         self.assertEqual(type(new.name), str)
+
+    def test_pep8(self):
+        """Check pep8 styling"""
+        p = pep8.StyleGuide(quiet=True).check_files(["models/state.py"])
+        self.assertEqual(p.total_errors, 0)
