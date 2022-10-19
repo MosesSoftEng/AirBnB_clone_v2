d5aa2627bf1c8f4c03d03c4a7ec3f1cdcc0a78fe
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-19 12:31:20 +0300

    a

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,13 +6,16 @@ Routes:
 """
 from flask import Flask
 
+# Create an instance of this Flask class passing the name of this module
+#   for location of other web files; templates.
 app = Flask(__name__)
 
-
+# Routes
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+# Set socket to run the app on only if accessed directly
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
